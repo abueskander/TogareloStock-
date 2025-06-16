9ac2485535074bb7b79f826c37cf6c0e8fd61e63
Mohamed Gamal Eldin <mohmed666651@gmail.com>
2025-06-17 02:50:51 +0300

    Create save_post.php

---
+++ save_post.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = strip_tags($_POST["title"]);
+    $content = strip_tags($_POST["content"]);
+    $author = strip_tags($_POST["author"]);
+
+    $line = "$title|$content|$author\n";
+    file_put_contents("posts.txt", $line, FILE_APPEND | LOCK_EX);
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+    exit();
+}
+?>
